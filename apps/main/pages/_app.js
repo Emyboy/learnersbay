2ea2999b76ff778e12da5155f0201df8d6bf885a
Emy Ifeora <emyboybeats@gmail.com>
2023-03-24 14:47:08 +0100

    added template

---
+++ apps/main/pages/_app.js
@@ -0,0 +1,6 @@
+import '../public/assets/css/vendors.css'
+import '../public/assets/css/main.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
