a4279dbef4c7cc7da4c517997291198fae248966
Emy Ifeora <emyboybeats@gmail.com>
2023-04-15 22:07:24 +0100

    fixed URL

--- apps/community/Globals.ts
+++ apps/community/Globals.ts
@@ -1,6 +1,6 @@
 export default {
     API_URL:
         process.env.NODE_ENV === 'production'
-            ? `learnersbayapi-production.up.railway.app`
+            ? `https://learnersbayapi-production.up.railway.app`
             : `http://localhost:1337`,
 };
